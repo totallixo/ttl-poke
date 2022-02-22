363c2ac5d0d8b17f2dcb3edd22fe24208572f8ff
totallixo <100052179+totallixo@users.noreply.github.com>
2022-02-22 20:55:44 -0300

    update final chega

--- script.js
+++ script.js
@@ -3,88 +3,86 @@ const pokeData = [
     nome: 'Hauter',
     nick: 'Luftal',
     type: 'Ghost - Poison',
-    img: 'https://archives.bulbagarden.net/media/upload/6/6d/Spr_5b_093.png',
+    img: '/imgs/haunter.png',
   },
   {
     nome: 'Crobat',
     nick: 'Aedes Egito',
     type: 'Poison - Flying',
-    img: 'https://archives.bulbagarden.net/media/upload/5/51/Spr_5b_169.png'
+    img: '/imgs/crowbat.png'
   },
- {
+  {
     nome: 'Ditto',
     nick: 'Não escolhido ainda',
     type: 'Normal',
-    img: 'https://archives.bulbagarden.net/media/upload/5/5f/Spr_5b_132.png',
+    img: '/imgs/ditto.png'
   },
   {
     nome: 'Mantine',
     nick: 'Claudia Arraia',
     type: 'Water - Flying',
-    img: 'https://archives.bulbagarden.net/media/upload/7/7b/Spr_5b_226.png'
+    img: '/imgs/mantine.png'
   },
   {
     nome: 'Noibat',
     nick: 'Punhetão',
     type: 'Flying - Dragon',
-    img: 'https://www.smogon.com/dex/media/sprites/xy/noibat.gif'
+    img: '/imgs/noibat.gif'
   },
   {
     nome: 'Espeon',
     nick: 'Churrasquinho',
     type: 'Psychic',
-    img: 'https://archives.bulbagarden.net/media/upload/0/07/Spr_5b_196.png'
+    img: '/imgs/espion.png'
   },
   {
     nome: 'Shellder',
     nick: 'Cleiton',
     type: 'Water',
-    img: 'https://archives.bulbagarden.net/media/upload/6/66/Spr_5b_090.png'
+    img: '/imgs/shelder.png'
   },
   {
     nome: 'Arbok',
     nick: 'Ekandidiase',
     type: 'Poison',
-    img: 'https://archives.bulbagarden.net/media/upload/0/00/Spr_5b_024.png'
+    img: '/imgs/arbok.png'
   },
   {
     nome: 'Crabrawler',
     nick: 'Lu Punhetinha',
     type: 'Fighting',
-    img: 'https://www.smogon.com/dex/media/sprites/xy/crabrawler.gif'
+    img: '/imgs/crabrawler.gif'
   },
   {
     nome: 'Tsareena',
     nick: 'Bulbassaura',
     type: 'Grass',
-    img: 'https://www.smogon.com/dex/media/sprites/xy/tsareena.gif'
+    img: '/imgs/tsareena.gif'
   },
   {
     nome: 'Marowak (Alolan)',
     nick: 'Cubona',
     type: 'Fire - Ghost',
-    img: 'https://www.smogon.com/dex/media/sprites/xy/marowak-alola.gif'
+    img: '/imgs/marowak-alola.gif'
   },
   {
     nome: 'Drifblim',
     nick: 'Prd do Balão',
     type: 'Ghost - Flying',
-    img: 'https://archives.bulbagarden.net/media/upload/3/32/Spr_5b_426.png'
+    img: '/imgs/driff.png'
   },
   {
     nome: 'Shiinotic',
     nick: 'PauDoMarcelo',
     type: 'Grass - Fairy',
-    img: 'https://www.smogon.com/dex/media/sprites/xy/shiinotic.gif'
+    img: '/imgs/shiinotic.gif'
   },
 ];
 
 const div = document.querySelector('.pokemons')
 
-
-console.log(pokeData)
-
 function add ({ nome, nick, tipo, img }) {
+  console.log(nome)
   const section = document.createElement('section')
   section.className = 'card';
   const name = document.createElement('p')
@@ -107,7 +105,7 @@ function add ({ nome, nick, tipo, img }) {
 
 function each () {
   pokeData.forEach((element) => {
-      const data = {
+    const data = {
       nome: element.nome,
       nick: element.nick,
       tipo: element.type,
@@ -120,12 +118,17 @@ function each () {
 window.onload = async () => {
   await each();
   console.log(`
-  ▄▄▄▄▀ ████▄    ▄▄▄▄▀ ██   █         █    ▄█     ▄  ████▄ 
-▀▀▀ █    █   █ ▀▀▀ █    █ █  █         █    ██ ▀▄   █ █   █ 
-   █    █   █     █    █▄▄█ █         █    ██   █ ▀  █   █ 
-  █     ▀████    █     █  █ ███▄      ███▄ ▐█  ▄ █   ▀████ 
- ▀              ▀         █     ▀         ▀ ▐ █   ▀▄       
-                         █                     ▀           
-                        ▀                                  
-`)
+  PARABÉNS VOCÊ ENCONTROU A:
+  
+▄▄▄█████▓ ▒█████  ▄▄▄█████▓ ▄▄▄       ██▓        ██▓     ██▓▒██   ██▒ ▒█████  
+▓  ██▒ ▓▒▒██▒  ██▒▓  ██▒ ▓▒▒████▄    ▓██▒       ▓██▒    ▓██▒▒▒ █ █ ▒░▒██▒  ██▒
+▒ ▓██░ ▒░▒██░  ██▒▒ ▓██░ ▒░▒██  ▀█▄  ▒██░       ▒██░    ▒██▒░░  █   ░▒██░  ██▒
+░ ▓██▓ ░ ▒██   ██░░ ▓██▓ ░ ░██▄▄▄▄██ ▒██░       ▒██░    ░██░ ░ █ █ ▒ ▒██   ██░
+  ▒██▒ ░ ░ ████▓▒░  ▒██▒ ░  ▓█   ▓██▒░██████▒   ░██████▒░██░▒██▒ ▒██▒░ ████▓▒░
+  ▒ ░░   ░ ▒░▒░▒░   ▒ ░░    ▒▒   ▓▒█░░ ▒░▓  ░   ░ ▒░▓  ░░▓  ▒▒ ░ ░▓ ░░ ▒░▒░▒░ 
+    ░      ░ ▒ ▒░     ░      ▒   ▒▒ ░░ ░ ▒  ░   ░ ░ ▒  ░ ▒ ░░░   ░▒ ░  ░ ▒ ▒░ 
+  ░      ░ ░ ░ ▒    ░        ░   ▒     ░ ░        ░ ░    ▒ ░ ░    ░  ░ ░ ░ ▒  
+             ░ ░                 ░  ░    ░  ░       ░  ░ ░   ░    ░      ░ ░  
+                                                                              
+BOA SORTE`)
 };
