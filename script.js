bcc8e52a95db46b1b06bdb5cd751a4ba122860fa
totallixo <100052179+totallixo@users.noreply.github.com>
2022-03-08 21:02:54 -0300

    coahcpihapcjaspodcpasojdçasjdpoaspojdas

--- script.js
+++ script.js
@@ -2,98 +2,112 @@ const pokeData = [
   {
     nome: 'Hauter',
     nick: 'Luftal',
-    type: './icons/Ghost.webp',
+    type1: './icons/Ghost.webp',
+    type2: './icons/Poison.webp',
     img: './imgs/haunter.png',
   },
   {
     nome: 'Crobat',
     nick: 'Aedes Egito',
-    type: 'Poison - Flying',
+    type1: './icons/Flying.webp',
+    type2: './icons/Poison.webp',
     img: './imgs/crowbat.png'
   },
   {
     nome: 'Ditto',
     nick: 'Jarbas',
-    type: 'Normal',
+    type1: './icons/Normal.webp',
+    type2: '',
     img: './imgs/ditto.png'
   },
   {
     nome: 'Mantine',
     nick: 'Claudia Arraia',
-    type: 'Water - Flying',
+    type1: './icons/Water.webp',
+    type2: './icons/Flying.webp',
     img: './imgs/mantine.png'
   },
   {
     nome: 'Noibat',
     nick: 'Punhetão',
-    type: 'Flying - Dragon',
+    type1: './icons/Flying.webp',
+    type2: './icons/Dragon.webp',
     img: './imgs/noibat.gif'
   },
   {
     nome: 'Espeon',
     nick: 'Churrasquinho',
-    type: 'Psychic',
+    type1: './icons/Psychic.webp',
+    type2: '',
     img: './imgs/espion.png'
   },
   {
     nome: 'Starmie',
     nick: 'PT',
-    type: 'Water - Psychic',
+    type1: './icons/Water.webp',
+    type2: './icons/Psychic.webp',
     img: './imgs/starmie.png',
   },
   {
     nome: 'Shellder',
     nick: 'Cleiton',
-    type: 'Water',
+    type1: './icons/Water.webp',
+    type2: '',
     img: './imgs/shelder.png'
   },
   {
     nome: 'Arbok',
     nick: 'Ekandidiase',
-    type: 'Poison',
+    type1: './icons/Poison.webp',
+    type2: '',
     img: './imgs/arbok.png'
   },
   {
     nome: 'Marowak (Alolan)',
     nick: 'Cubona',
-    type: 'Fire - Ghost',
+    type1: './icons/Fire.webp',
+    type2: './icons/Ghost.webp',
     img: './imgs/marowak-alola.gif'
   },
   {
     nome: 'Crabrawler',
     nick: 'Lu Punhetinha',
-    type: 'Fighting',
+    type1: './icons/Fighting.webp',
     img: './imgs/crabrawler.gif'
   },
   {
     nome: 'Shiinotic',
     nick: 'PauDoMarcelo',
-    type: 'Grass - Fairy',
+    type1: './icons/Grass.webp',
+    type2: './icons/Fairy.webp',
     img: './imgs/shiinotic.gif'
   },
   {
     nome: 'Tsareena',
     nick: 'Bulbassaura',
-    type: 'Grass',
+    type1: './icons/Grass.webp',
+    type2: '',
     img: './imgs/tsareena.gif'
   },
   {
     nome: 'Goomy',
     nick: 'Bukkakemon',
-    type: 'Water - Psychic',
+    type1: './icons/Water.webp',
+    type2: './icons/Psychic.webp',
     img: './imgs/goomy.gif',
   },
   {
     nome: 'Drifblim',
     nick: 'Prd do Balão',
-    type: 'Ghost - Flying',
+    type1: './icons/Ghost.webp',
+    type2: './icons/Flying.webp',
     img: './imgs/driff.png'
   },
 ];
 
 const div = document.querySelector('.pokemons')
 
-function add ({ nome, nick, tipo, img }) {
+function add ({ nome, nick, tipo1, tipo2, img }) {
   console.log(nome)
   const section = document.createElement('section')
   section.className = 'card';
@@ -103,9 +117,12 @@ function add ({ nome, nick, tipo, img }) {
   const nickName = document.createElement('p')
   nickName.innerHTML = nick;
   nickName.className = 'nick';
-  const type = document.createElement('img')
-  type.src = tipo;
-  type.className = 'type';
+  const type1 = document.createElement('img')
+  type.src = tipo1;
+  type1.className = 'type';
+  const type2 = document.createElement('img')
+  type2.src = tipo2;
+  type2.className = 'type';
   const imagem = document.createElement('img')
   imagem.src = img;
 
@@ -121,7 +138,8 @@ function each () {
     const data = {
       nome: element.nome,
       nick: element.nick,
-      tipo: element.type,
+      tipo1: element.type1,
+      tipo2: element.type2,
       img: element.img,
     }
     div.appendChild(add(data));
